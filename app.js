2dfe6865dafa3494ea32ef33d64cc5db480292b8
Oscar Elizondo <oscarmarcelo95@gmail.com>
2015-03-16 19:09:25 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var app = angular.module('fModule', []);
+	app.controller('FirstCtrl', function($scope){
+		$scope.title = 'Arigato';
+		$scope.test = 'Testing in different divs';
+		$scope.presentacion = 'La mejor apliación para catadores de cerveza';
+
+	});
